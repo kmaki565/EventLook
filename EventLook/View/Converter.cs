04cf3267cbe4788b55f9b260e0c56fb289c64599
K. Maki <kmaki565@gmail.com>
2025-04-20 22:19:23 +0900

    Gray out ID filter context menu when already applied

--- EventLook/View/Converter.cs
+++ EventLook/View/Converter.cs
@@ -248,3 +248,19 @@ public class SelectedEventsTextConverter : IValueConverter
         throw new NotImplementedException();
     }
 }
+
+public class NullableToInverseBooleanConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is null)
+            return true;
+        else
+            return false;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
